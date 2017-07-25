c67d8d62c5392ca6c09d6ce6492a360583f70012
Vladimir Pavlov <kolobokhtc@gmail.com>
2017-07-25 12:19:16 +0300

    add entry listener

---
+++ src/Api/Event/Entry.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vladimir Pavlov <kolobokhtc@gmail.com>
+ * Date: 24.07.2017 13:06
+ */
+
+namespace FacebookBot\Api\Event;
+
+
+use FacebookBot\Api\Event;
+
+class Entry extends Event
+{
+
+    public function __construct(array $data)
+    {
+
+        $this->event = new \FacebookBot\Api\Entry($data);
+
+    }
+
+
+}
